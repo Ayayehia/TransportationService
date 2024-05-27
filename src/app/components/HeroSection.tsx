45d02953d1ff6143ed89dd0eafead58a6598e819
ayayehia <ayanader8@gmail.com>
2024-05-27 15:12:38 +0300

    Hero Section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -9,12 +9,12 @@ const HeroSection = () => {
       />
 
       <div className="absolute inset-0 flex flex-col justify-center items-center">
-        <h2 className=" text-center lg:text-start uppercase lg:font-bold sm:text-4xl text-lg lg:text-6xl text-white lg:min-w-[940px]">
+        <h2 className=" text-center lg:text-start uppercase lg:font-bold sm:text-4xl text-lg lg:text-5xl text-white lg:min-w-[940px] lg:max-h-[184px] xl:text-6xl">
           <span className="block text-[#E7C13C]">Premium</span>
           Transpotation <br className="hidden lg:block" />
           Services
         </h2>
-        <div className="flex flex-row lg:flex-col lg:min-w-[940px] lg:mt-20 mt-4 gap-14 underline text-white underline-offset-4 lg:text-2xl font-medium">
+        <div className="flex flex-row lg:flex-col lg:min-w-[940px] xl:mt-20 mt-4 gap-14 underline text-white underline-offset-4 lg:text-2xl font-medium">
           <Link href="/qoute">Get a Quote</Link>
           <Link href="/booknow">Book Now</Link>
         </div>
