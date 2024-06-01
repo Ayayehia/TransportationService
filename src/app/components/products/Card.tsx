888df9786960d7c2c6a22b2f67d1e7bbf17bb3a4
ayayehia <ayanader8@gmail.com>
2024-06-01 18:04:03 +0300

    Car Models updtaed

--- src/app/components/products/Card.tsx
+++ src/app/components/products/Card.tsx
@@ -1,21 +1,50 @@
+import Link from "next/link";
 const Card = ({
   type,
   model,
   passengers,
   suitcases,
+  image,
 }: {
   type: string;
   model: string;
   passengers: string;
   suitcases: string;
+  image: string;
 }) => {
   return (
-    <div className="rounded-lg border-[#E6EBEE] border-2 uppercase ">
+    // <div className="relative group">
+    //   <div className="rounded-lg border-[#E6EBEE] border-2 uppercase overflow-hidden group-hover:bg-yellow-500 transition duration-300 ease-in-out">
+    //     <div className="flex flex-col justify-center items-center w-full pt-9 group-hover:opacity-0 transition duration-300 ease-in-out">
+    //       <p className="text-[#B4C6D0] text-lg">{type}</p>
+    //       <h2 className="text-lg text-[#778892] font-semibold">{model}</h2>
+    //       <div className="border-b-[#E7C13C] border-b-2 w-1/2 my-2" />
+    //       <img
+    //         src="/car_cadillac_escalade-328x194.jpg.png"
+    //         className="w-full"
+    //         alt={`${model}`}
+    //       />
+    //       <p className="text-lg">
+    //         Passengers:{" "}
+    //         <span className="font-medium text-2xl">{passengers}</span>
+    //       </p>
+    //       <p className="text-lg pb-9">
+    //         Suitcases: <span className="font-medium text-2xl">{suitcases}</span>
+    //       </p>
+    //     </div>
+    //     <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+    //       <span className="text-2xl font-bold uppercase text-white">
+    //         Reserve Now
+    //       </span>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="rounded-lg border-[#E6EBEE] border-2 uppercase text-center ">
       <div className="flex flex-col justify-center items-center w-full pt-9">
         <p className="text-[#B4C6D0] text-lg">{type}</p>
         <h2 className="text-lg text-[#778892] font-semibold ">{model}</h2>
         <div className="border-b-[#E7C13C] border-b-4  " />
-        <img src="/car_cadillac_escalade-328x194.jpg.png" className="w-full" />
+        <img src={image} className="w-fit h-[155px]" />
         <p className="text-lg">
           Passengers:
           <span className="font-medium text-2xl">{passengers}</span>
@@ -24,7 +53,7 @@ const Card = ({
           Suitcases:<span className="font-medium text-2xl">{suitcases}</span>
         </p>
         <button className="py-8 w-full bg-[#E6EBEE] text-[#778892] font-medium text-sm uppercase">
-          Reserve Now
+          <Link href="/reserve">Reserve Now</Link>
         </button>
       </div>
     </div>
--- src/app/main/fleet/page.tsx
+++ src/app/main/fleet/page.tsx
@@ -6,76 +6,87 @@ const page = () => {
       model: "Cadillac Escalade ESV",
       passengers: "6",
       suitcases: "6",
+      image: "/car_cadillac_escalade-328x194.jpg.png",
     },
     {
       type: "Executive SUV",
       model: "Mercedes GLS",
       passengers: "6",
       suitcases: "6",
+      image: "/car_mersedes_gls.png.png",
     },
     {
       type: "Premium Sedan",
       model: "Cadillac CT6 or Similar",
       passengers: "1-3",
       suitcases: "2",
+      image: "/car_cadillac_ct6-328x158.jpg.png",
     },
     {
       type: "Executive Sedan",
       model: "BMW X1",
       passengers: "1-3",
       suitcases: "4",
+      image: "/bmw_x1-328x198.jpg.png",
     },
     {
       type: "Limousine Van",
       model: "Mercedes Sprinter or Similar",
       passengers: "11-14",
       suitcases: "8",
+      image: "/car_mercedes_sprinter-328x202.jpg.png",
     },
     {
       type: "Executive Van",
       model: "Ford Transit Van",
       passengers: "7-14",
       suitcases: "14",
+      image: "/car_cadillac_escalade-328x194.jpg.png",
     },
     {
       type: "Specialty Limousine",
       model: "Cadillac Limousine or Similar",
       passengers: "Up to 15",
       suitcases: "—",
+      image: "/SPECIALTY_LIMO-328x164.jpg.png",
     },
     {
       type: "Stretch Limo",
       model: "MKT Limousine or Similar",
       passengers: "6-10",
       suitcases: "—",
+      image: "/flt1-328x181.jpg.png",
     },
     {
       type: "Minibus",
       model: "Freightliner Minibus",
       passengers: "24-38",
       suitcases: "25+",
+      image: "/freightliner.png",
     },
     {
       type: "Party Bus",
       model: "Party Bus",
       passengers: "30",
       suitcases: "—",
+      image: "/party_bus.jpg.png",
     },
     {
       type: "Executive Motor Coach",
       model: "Motorcoach",
       passengers: "57",
       suitcases: "57+",
+      image: "/MOTORCOACH-328x203.png.png",
     },
   ];
 
   return (
-    <div className="xl:px-[200px] p-4 lg:p-0 lg:px-[100px]">
+    <div className="xl:px-[200px] p-4 lg:p-0 lg:px-[100px] ">
       <div className="lg:pt-12 pt-6">Home/Fleet</div>
       <div className="flex w-full items-center justify-center">
         <h3 className="text-4xl uppercase lg:pt-[132px] pt-8">Fleet</h3>
       </div>
-      <div className=" grid grid-cols-1 lg:grid-cols-3 my-9  gap-y-5 gap-x-3">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-9  gap-y-5 gap-x-3">
         {cars.map((items) => {
           return (
             <Card
@@ -83,6 +94,7 @@ const page = () => {
               model={items.model}
               passengers={items.passengers}
               suitcases={items.suitcases}
+              image={items.image}
             />
           );
         })}
