bf5d11a979af5755f742503acc97ecd6d51447ad
ayayehia <ayanader8@gmail.com>
2024-06-01 23:47:27 +0300

    fixing bug in services

--- src/app/main/services/page.tsx
+++ src/app/main/services/page.tsx
@@ -1,39 +1,56 @@
 import Card from "@/app/components/layout/services/Card";
 const page = () => {
   const services = [
-    { title: "Airport Limo Service", step: 1, image: "/Airportservice.png" },
     {
+      id: 1,
+      title: "Airport Limo Service",
+      step: 1,
+      image: "/Airportservice.png",
+    },
+    {
+      id: 2,
       title: "Private Transportation",
       step: 2,
       image: "/Privatetransportation.png",
     },
     {
+      id: 3,
       title: "Corporate Transportation Service",
       step: 3,
       image: "/Corporatetransportaion.png",
     },
     {
+      id: 4,
       title: "Hourly and Daily Service",
       step: 4,
       image: "/Hourly&dailyservice.png",
     },
     {
+      id: 5,
       title: "Long Distance Service",
       step: 5,
       image: "/Longdistanceservice.png",
     },
-    { title: "14 Passengers Van Service", step: 6, image: "/Passengervan.png" },
     {
+      id: 6,
+      title: "14 Passengers Van Service",
+      step: 6,
+      image: "/Passengervan.png",
+    },
+    {
+      id: 7,
       title: "Female Drive Service",
       step: 7,
       image: "/Femaledriver.png",
     },
     {
+      id: 8,
       title: "Orlando - Miami Limo Service",
       step: 8,
       image: "/Orlando.png",
     },
     {
+      id: 9,
       title: "Miami - Key West Transportation Service",
       step: 9,
       image: "/Miami.png",
@@ -51,7 +68,9 @@ const page = () => {
       <div className="flex flex-col gap-5  p-2 justify-center items-center pt-8">
         {services.map((items) => {
           return (
-            <Card title={items.title} image={items.image} step={items.step} />
+            <div key={items.id}>
+              <Card title={items.title} image={items.image} step={items.step} />
+            </div>
           );
         })}
       </div>
